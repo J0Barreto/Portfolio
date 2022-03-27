93764c73e10933f5c58a52ded8dab41ba85c9b76
Jose <barretojantonio@gmail.com>
2022-03-27 20:58:23 -0300

    archivos al 270322

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+ScrollReveal().reveal('.info-perfil');
+ScrollReveal().reveal('.portada2', {delay: 500});
+ScrollReveal().reveal('.estudios', {delay: 500});
+ScrollReveal().reveal('.portada3', {delay: 500});
+ScrollReveal().reveal('.trabajo', {delay: 500});
+ScrollReveal().reveal('.social', {delay: 500});
+ScrollReveal().reveal('.habilidades', {delay: 500});
+   
